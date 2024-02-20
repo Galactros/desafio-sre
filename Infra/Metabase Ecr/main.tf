02af2a93634948313a35af05e44e361514d8fc57
Galactros <xtsjamal@gmail.com>
2024-02-20 17:58:21 -0300

    Terraform Metabase ECR
    Adicionando terraform para Ecr do metabase

---
+++ Infra/Metabase Ecr/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = local.region
+}
+
+locals {
+  region = "us-east-1"
+  name   = "desafio-sre"
+
+  tags = {
+    Name       = local.name
+
+  }
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_partition" "current" {}
+
+################################################################################
+# ECR Repository
+################################################################################
+
+module "ecr" {
+  source = "terraform-aws-modules/ecr/aws"
+
+  repository_name = "ecr-${local.name}"
+
+  repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
+  create_lifecycle_policy           = true
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 10 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 10
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  repository_force_delete = true
+
+  tags = local.tags
+}
