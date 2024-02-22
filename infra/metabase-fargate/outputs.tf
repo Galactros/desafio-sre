21221060cda2a3c3322e552dd10e3ef35f54b32a
Galactros <xtsjamal@gmail.com>
2024-02-22 06:39:17 -0300

    Removendo outputs para o record do route53

--- infra/metabase-fargate/outputs.tf
+++ infra/metabase-fargate/outputs.tf
@@ -2,8 +2,3 @@ output "route53_zone_name_servers" {
   description = "Name servers of Route53 zone"
   value       = module.zones.route53_zone_name_servers
 }
-
-output "route53_record_name" {
-  description = "The name of the record"
-  value       = module.records.route53_record_name
-}
